4d3b4c47fcce9f4458ebeb13a0273076ac5c0b85
BleuSkell <kyasowi1@gmail.com>
2025-05-21 15:20:44 +0200

    navbar styling in progress

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import '../css/NavBar.css';
 
 function NavBar() {
     return (
-        <nav style={{ marginBottom: "2rem" }}>
-            <Link to="/" style={{ marginRight: "1rem" }}>Homepage</Link>
+        <nav className="navbar">
+            <img src="../../public/img/TPCIA-logo.png" alt="TPCIA logo" className="TPCIA-logo"/>
+            <Link to="/">Homepage</Link>
             <Link to="/artlist">Artlist</Link>
+            <Link to="/">Favorites</Link>
+            <Link to="/">News</Link>
         </nav>
     );
 }
